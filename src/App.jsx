b3c4911a3aeda0a014f669ecc053deb51a388b3a
Muhamad Renald <avryso@gmail.com>
2025-02-18 17:19:13 +0700

    feat: update file App.jsx, file Navbar.jsx & css, file index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
+import { About } from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
+import Testimonials from "./Components/Testimonials/Testimonials";
 
 export const App = () => {
   return (
@@ -12,6 +15,12 @@ export const App = () => {
       <div className="container">
         <Title title="What We Offer" subTitle="Our Programs" />
         <Programs />
+        <About></About>
+        <Title title="Campus Photos" subTitle="Gallery" />
+        <Campus />
+        <Title title="What Student Says " subTitle="Testimonials" />
+        <Testimonials />
+        <Title title="Get In Touch" subTitle="Contact Us" />
       </div>
     </div>
   );
